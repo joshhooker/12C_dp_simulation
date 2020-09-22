766faa0295b55164d9b8e9d4d34f536e779ca142
Josh Hooker <joshualeehooker@gmail.com>
2020-09-22 08:35:44 -0400

    Fixed writing to file in bayesopt script

--- bayesian-optimization/bayesopt_energy_prob_error.py
+++ bayesian-optimization/bayesopt_energy_prob_error.py
@@ -219,42 +219,42 @@ def find_parameter_errors(max_parameters, num_parameters):
 
     file_ex1 = open('ex1_parameter_estimation.dat', "w+")
     for i in range(len(ex1_energy)):
-        file_ex1.write('%s %s\n', ex1_energy[i], ex1_chi2[i])
+        file_ex1.write('%s %s\n' % (ex1_energy[i], ex1_chi2[i]))
     file_ex1.close()
 
     file_ex2 = open('ex2_parameter_estimation.dat', "w+")
     for i in range(len(ex2_energy)):
-        file_ex2.write('%s %s\n', ex2_energy[i], ex2_chi2[i])
+        file_ex2.write('%s %s\n' % (ex2_energy[i], ex2_chi2[i]))
     file_ex2.close()
 
     file_ex3 = open('ex3_parameter_estimation.dat', "w+")
     for i in range(len(ex3_energy)):
-        file_ex3.write('%s %s\n', ex3_energy[i], ex3_chi2[i])
+        file_ex3.write('%s %s\n' % (ex3_energy[i], ex3_chi2[i]))
     file_ex3.close()
 
     file_ex4 = open('ex4_parameter_estimation.dat', "w+")
     for i in range(len(ex4_energy)):
-        file_ex4.write('%s %s\n', ex4_energy[i], ex4_chi2[i])
+        file_ex4.write('%s %s\n' % (ex4_energy[i], ex4_chi2[i]))
     file_ex4.close()
 
     file_p_ex1 = open('p_ex1_parameter_estimation.dat', "w+")
     for i in range(len(p_ex1_energy)):
-        file_p_ex1.write('%s %s\n', p_ex1_energy[i], p_ex1_chi2[i])
+        file_p_ex1.write('%s %s\n' % (p_ex1_energy[i], p_ex1_chi2[i]))
     file_p_ex1.close()
 
     file_p_ex2 = open('p_ex2_parameter_estimation.dat', "w+")
     for i in range(len(p_ex2_energy)):
-        file_p_ex2.write('%s %s\n', p_ex2_energy[i], p_ex2_chi2[i])
+        file_p_ex2.write('%s %s\n' % (p_ex2_energy[i], p_ex2_chi2[i]))
     file_p_ex2.close()
 
     file_p_ex3 = open('p_ex3_parameter_estimation.dat', "w+")
     for i in range(len(p_ex3_energy)):
-        file_p_ex3.write('%s %s\n', p_ex3_energy[i], p_ex3_chi2[i])
+        file_p_ex3.write('%s %s\n' % (p_ex3_energy[i], p_ex3_chi2[i]))
     file_p_ex3.close()
 
     file_p_ex4 = open('p_ex4_parameter_estimation.dat', "w+")
     for i in range(len(p_ex4_energy)):
-        file_p_ex4.write('%s %s\n', p_ex4_energy[i], p_ex4_chi2[i])
+        file_p_ex4.write('%s %s\n' % (p_ex4_energy[i], p_ex4_chi2[i]))
     file_p_ex4.close()
 
 if __name__ == '__main__':
